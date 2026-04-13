0ea860cc17deee6ac41eee99901f1f4e5efd280b
PugnatorStrenuus <leonardo.geodesi@gmail.com>
2026-04-13 11:59:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-const buttons_show = document.querySelectorAll():
+function showp() {
+  const tid = this.getAttribute('data-target');
+  const tel = document.getElementById(tid);
+
+  if (tel) {
+    tel.style.display='block';
+  } 
+}
+
+const buttons_show = document.querySelectorAll('.show');
+
+buttons_show.forEach(button => {
+ button.addEventListener('click', showp);
+})
