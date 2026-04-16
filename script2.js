1d2e81648c7f746eabb87030b9c055c084d0235a
PugnatorStrenuus <leonardo.geodesi@gmail.com>
2026-04-16 10:05:25 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,1 +1,15 @@
+function open_button() {
+  const tid = this.getAttribute('data-target');
+  const tel = document.getElementById(tid);
 
+  if (tel) {
+    tel.style.display='block';
+  }
+}
+
+const buttons_show = document.querySelectorAll('.show');
+
+button_show.forEach(button => {
+  button.addEventListener('click', open_button);
+  }
+)
